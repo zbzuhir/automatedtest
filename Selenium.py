d3b7cfd06d72975d37311c984fe5ba3e4a37c2c4
zuliana <zbzuhir@gmail.com>
2022-06-16 17:04:41 +0800

    add Driver.py file

---
+++ Selenium.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+import time
+
+# Awesome fonts < class fa fa-users > Change about us logo from apple logo to about us
+# To download Selenium web drivers
+# https://selenium-python.readthedocs.io/installation.html
